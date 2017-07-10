9af902d2563f0385bb430cc9c4c5049f578daccc
Mike Aizatsky <maizatskyi@mz.com>
2017-07-10 16:27:28 -0700

    migrating video-js to librtmvideo
    FREEZE.unindexed

--- cmake/ffmpeg.cmake
+++ cmake/ffmpeg.cmake
@@ -2,9 +2,12 @@ include(ExternalProject)
 
 set(FFMPEG_PREFIX ${CMAKE_BINARY_DIR}/ffmpeg)
 
+set(FFMPEG_CONFIGURE_COMMAND ${FFMPEG_PREFIX}/src/project_ffmpeg/configure)
+set(FFMPEG_MAKE_COMMAND make)
 set(FFMPEG_CONFIGURE_ARGS
         --cc=${CMAKE_C_COMPILER} --cxx=${CMAKE_CXX_COMPILER}
         --prefix=${FFMPEG_PREFIX}
+        --enable-pic
         "--extra-ldflags=-L${LIBVPX_LIB_DIR} -lvpx" --extra-cflags=-I${LIBVPX_INCLUDE_DIR}
         --enable-pic
         --disable-all --disable-programs --disable-everything --disable-sdl2
@@ -14,6 +17,22 @@ set(FFMPEG_CONFIGURE_ARGS
         --enable-decoder=h264 --enable-decoder=mjpeg
         --enable-decoder=libvpx_vp9 --enable-encoder=libvpx_vp9
     )
+    
+set(FFMPEG_PATCH_COMMAND )
+set(FFMPEG_LIB_SUFFIX a)
+
+IF(${CMAKE_SYSTEM_NAME} MATCHES "Emscripten")
+    message("    ffmpeg: Emscripten")
+    set(FFMPEG_CONFIGURE_COMMAND emconfigure ${FFMPEG_CONFIGURE_COMMAND})
+    set(FFMPEG_MAKE_COMMAND emmake ${FFMPEG_MAKE_COMMAND})
+    set(FFMPEG_CONFIGURE_ARGS ${FFMPEG_CONFIGURE_ARGS}
+            --enable-cross-compile --target-os=none --arch=x86  --disable-asm --disable-runtime-cpudetect
+            --disable-fast-unaligned --disable-pthreads
+            --disable-static --enable-shared --disable-stripping
+        )
+    set(FFMPEG_PATCH_COMMAND patch -p1 < ${CMAKE_SOURCE_DIR}/../cmake/ffmpeg-js.patch)
+    set(FFMPEG_LIB_SUFFIX so)
+ENDIF()
 
 IF(CMAKE_BUILD_TYPE MATCHES Debug)
     message("    ffmpeg: Debug")
@@ -27,16 +46,16 @@ ExternalProject_Add(
         GIT_REPOSITORY https://github.com/FFmpeg/FFmpeg.git
         GIT_TAG n3.3.1
         UPDATE_COMMAND ""
-        CONFIGURE_COMMAND
-            ${FFMPEG_PREFIX}/src/project_ffmpeg/configure ${FFMPEG_CONFIGURE_ARGS}
-        BUILD_COMMAND V=1 make -j8
-        INSTALL_COMMAND V=1 make install
+        PATCH_COMMAND ${FFMPEG_PATCH_COMMAND}
+        CONFIGURE_COMMAND ${FFMPEG_CONFIGURE_COMMAND} ${FFMPEG_CONFIGURE_ARGS}
+        BUILD_COMMAND V=1 ${FFMPEG_MAKE_COMMAND} -j8
+        INSTALL_COMMAND ${FFMPEG_MAKE_COMMAND} install
         BUILD_BYPRODUCTS 
-            ${FFMPEG_PREFIX}/lib/libavcodec.a
-            ${FFMPEG_PREFIX}/lib/libavutil.a
-            ${FFMPEG_PREFIX}/lib/libavdevice.a
-            ${FFMPEG_PREFIX}/lib/libavformat.a
-            ${FFMPEG_PREFIX}/lib/libswscale.a            
+            ${FFMPEG_PREFIX}/lib/libavcodec.${FFMPEG_LIB_SUFFIX}
+            ${FFMPEG_PREFIX}/lib/libavutil.${FFMPEG_LIB_SUFFIX}
+            ${FFMPEG_PREFIX}/lib/libavdevice.${FFMPEG_LIB_SUFFIX}
+            ${FFMPEG_PREFIX}/lib/libavformat.${FFMPEG_LIB_SUFFIX}
+            ${FFMPEG_PREFIX}/lib/libswscale.${FFMPEG_LIB_SUFFIX}
 )
 
 set(FFMPEG_FOUND 1)
@@ -44,20 +63,20 @@ set(FFMPEG_INCLUDE_DIR ${FFMPEG_PREFIX}/include/)
 
 add_library(avcodec STATIC IMPORTED)
 add_dependencies(avcodec project_ffmpeg)
-set_property(TARGET avcodec PROPERTY IMPORTED_LOCATION ${FFMPEG_PREFIX}/lib/libavcodec.a)
+set_property(TARGET avcodec PROPERTY IMPORTED_LOCATION ${FFMPEG_PREFIX}/lib/libavcodec.${FFMPEG_LIB_SUFFIX})
 
 add_library(avutil STATIC IMPORTED)
 add_dependencies(avutil project_ffmpeg)
-set_property(TARGET avutil PROPERTY IMPORTED_LOCATION ${FFMPEG_PREFIX}/lib/libavutil.a)
+set_property(TARGET avutil PROPERTY IMPORTED_LOCATION ${FFMPEG_PREFIX}/lib/libavutil.${FFMPEG_LIB_SUFFIX})
 
 add_library(avdevice STATIC IMPORTED)
 add_dependencies(avdevice project_ffmpeg)
-set_property(TARGET avdevice PROPERTY IMPORTED_LOCATION ${FFMPEG_PREFIX}/lib/libavdevice.a)
+set_property(TARGET avdevice PROPERTY IMPORTED_LOCATION ${FFMPEG_PREFIX}/lib/libavdevice.${FFMPEG_LIB_SUFFIX})
 
 add_library(avformat STATIC IMPORTED)
 add_dependencies(avformat project_ffmpeg)
-set_property(TARGET avformat PROPERTY IMPORTED_LOCATION ${FFMPEG_PREFIX}/lib/libavformat.a)
+set_property(TARGET avformat PROPERTY IMPORTED_LOCATION ${FFMPEG_PREFIX}/lib/libavformat.${FFMPEG_LIB_SUFFIX})
 
 add_library(swscale STATIC IMPORTED)
 add_dependencies(swscale project_ffmpeg)
-set_property(TARGET swscale PROPERTY IMPORTED_LOCATION ${FFMPEG_PREFIX}/lib/libswscale.a)
+set_property(TARGET swscale PROPERTY IMPORTED_LOCATION ${FFMPEG_PREFIX}/lib/libswscale.${FFMPEG_LIB_SUFFIX})
--- cmake/libvpx.cmake
+++ cmake/libvpx.cmake
@@ -8,6 +8,19 @@ set(LIBVPX_CONFIGURE_ARGS --prefix=${LIBVPX_PREFIX}/install/
         --enable-pic --disable-examples --disable-tools --disable-docs
         --disable-multithread)
 
+set(LIBVPX_CONFIGURE_COMMAND ./configure)
+set(LIBVPX_MAKE_COMMAND make)
+set(LIBVPX_LIB_SUFFIX a)
+
+IF(${CMAKE_SYSTEM_NAME} MATCHES "Emscripten")
+    message("    libvpx: Emscripten")
+    set(LIBVPX_CONFIGURE_ARGS ${LIBVPX_CONFIGURE_ARGS}
+            --target=generic-gnu  --disable-static --enable-shared)
+    set(LIBVPX_CONFIGURE_COMMAND emconfigure ./configure)
+    set(LIBVPX_MAKE_COMMAND emmake make)
+    set(LIBVPX_LIB_SUFFIX so)
+ENDIF()
+
 IF(CMAKE_BUILD_TYPE MATCHES Debug)
     message("    libvpx: Debug")
     set(LIBVPX_CONFIGURE_ARGS ${LIBVPX_CONFIGURE_ARGS}
@@ -20,10 +33,10 @@ ExternalProject_Add(
         GIT_REPOSITORY https://chromium.googlesource.com/webm/libvpx
         GIT_TAG v1.6.1
         UPDATE_COMMAND ""
-        CONFIGURE_COMMAND ./configure ${LIBVPX_CONFIGURE_ARGS}
-        BUILD_COMMAND V=1 make -j8
-        INSTALL_COMMAND V=1 make install
-        BUILD_BYPRODUCTS ${LIBVPX_PREFIX}/install/lib/libvpx.a
+        CONFIGURE_COMMAND ${LIBVPX_CONFIGURE_COMMAND} ${LIBVPX_CONFIGURE_ARGS}
+        BUILD_COMMAND V=1 ${LIBVPX_MAKE_COMMAND} -j8
+        INSTALL_COMMAND make install
+        BUILD_BYPRODUCTS ${LIBVPX_PREFIX}/install/lib/libvpx.${LIBVPX_LIB_SUFFIX}
         BUILD_IN_SOURCE 1
 )
 
@@ -32,5 +45,5 @@ set(LIBVPX_INCLUDE_DIR ${LIBVPX_PREFIX}/install/include/)
 
 add_library(libvpx STATIC IMPORTED)
 add_dependencies(libvpx project_libvpx)
-set_property(TARGET libvpx PROPERTY IMPORTED_LOCATION ${LIBVPX_PREFIX}/install/lib/libvpx.a)
+set_property(TARGET libvpx PROPERTY IMPORTED_LOCATION ${LIBVPX_PREFIX}/install/lib/libvpx.${LIBVPX_LIB_SUFFIX})
 set(LIBVPX_LIBS libvpx)
