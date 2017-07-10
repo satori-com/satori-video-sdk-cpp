0459cdd3064c3983d423a34fb17e8a40e8a7e193
Renat Idrisov <ridrisov@mz.com>
2017-07-10 15:14:40 -0700

    CR fix

--- include/librtmvideo/rtmvideo.h
+++ include/librtmvideo/rtmvideo.h
@@ -4,7 +4,7 @@
 
 enum class image_pixel_format { RGB0 = 1, BGR = 2 };
 
-constexpr char message_channel_suffix[] = "/message";
+constexpr char message_channel_suffix[] = "/control";
 constexpr char metadata_channel_suffix[] = "/metadata";
 constexpr char analysis_channel_suffix[] = "/analysis";
 constexpr char debug_channel_suffix[] = "/debug";
