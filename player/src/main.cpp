280bd3a047499c84b4d27ace41c42643b94d4aee
Renat Idrisov <ridrisov@mz.com>
2017-07-14 10:47:29 -0700

    Simple library-based videoplayer

---
+++ player/src/main.cpp
@@ -0,0 +1,21 @@
+#include <assert.h>
+#include <librtmvideo/video_bot.h>
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+namespace player {
+void process_image(bot_context &context, const uint8_t *image, uint16_t width,
+                   uint16_t height, uint16_t linesize) {
+  // Read image
+  cv::Mat original(height, width, CV_8UC3, (void *)image, linesize);
+  cv::imshow("Player", original);
+  cv::waitKey(10);
+}
+}  // namespace player
+
+int main(int argc, char *argv[]) {
+  cv::namedWindow("Player");
+  rtm_video_bot_register(bot_descriptor{640, 480, image_pixel_format::BGR,
+                                        &player::process_image, nullptr});
+  return rtm_video_bot_main(argc, argv);
+}
