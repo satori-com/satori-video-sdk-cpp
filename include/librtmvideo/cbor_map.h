649d565c9a838b2aec5f14e7349fdaa424873791
Renat Idrisov <ridrisov@mz.com>
2017-07-21 13:21:03 -0700

    Fixing strings because they are non zero terminating

--- include/librtmvideo/cbor_map.h
+++ include/librtmvideo/cbor_map.h
@@ -3,9 +3,11 @@
 #include <cbor.h>
 
 extern "C" {
-// Returns string value for a key or default value if key is not found or not a string.
-const char *cbor_map_get_str(cbor_item_t *map, const char *name,
-                             const char *default_value = nullptr);
+// Returns string value for a key or default value if key is not found or not a
+// string.
+void cbor_map_get_str(const char **out_handle, size_t *out_length,
+                      cbor_item_t *map, const char *name,
+                      const char *default_value = nullptr);
 
 // Returns boolean flag indicating if key/value pair exists.
 int cbor_map_has_str_value(cbor_item_t *map, const char *name,
--- src/cbor_map.cpp
+++ src/cbor_map.cpp
@@ -18,15 +18,23 @@ int cbor_map_has_str_value(cbor_item_t *map, const char *name,
                            const char *value) {
   cbor_item_t *item = cbor_map_get(map, name);
   if (item == nullptr) return 0;
-  if (strcmp(value, reinterpret_cast<char *>(cbor_string_handle(item))) == 0) {
+  if (cbor_string_length(item) != strlen(value)) return 0;
+  if (strncmp(value, reinterpret_cast<char *>(cbor_string_handle(item)),
+              cbor_string_length(item)) == 0) {
     return 1;
   }
   return 0;
 }
 
-const char *cbor_map_get_str(cbor_item_t *map, const char *name,
-                             const char *default_value) {
+void cbor_map_get_str(const char **out_handle, size_t *out_length,
+                      cbor_item_t *map, const char *name,
+                      const char *default_value) {
   cbor_item_t *value = cbor_map_get(map, name);
-  if (value == nullptr) return default_value;
-  return reinterpret_cast<char *>(cbor_string_handle(value));
+  if (value == nullptr) {
+    *out_handle = default_value;
+    *out_length = strlen(default_value);
+    return;
+  }
+  *out_handle = reinterpret_cast<char *>(cbor_string_handle(value));
+  *out_length = cbor_string_length(value);
 }
