ec8d101d60ada75ae2de1bc53bf16cc06bebd53e
Renat Idrisov <ridrisov@mz.com>
2017-07-24 11:18:38 -0700

    More CR fixes

--- include/librtmvideo/cbor_map.h
+++ include/librtmvideo/cbor_map.h
@@ -6,14 +6,14 @@
 namespace cbor_helpers {
 // Returns string value for a key or default value if key is not found or not a
 // string.
-const std::string map_get_str(cbor_item_t *map, const char *name,
-                                   const std::string default_value);
+std::string map_get_str(cbor_item_t *map, const std::string name,
+                        const std::string default_value);
 
 // Returns boolean flag indicating if key/value pair exists.
-bool map_has_str_value(cbor_item_t *map, const char *name,
-                            const char *value);
+bool map_has_str_value(cbor_item_t *map, const std::string name,
+                       const std::string value);
 
 // Returns CBOR value for a key or default value if key is not found.
-cbor_item_t *map_get(cbor_item_t *map, const char *name,
-                          cbor_item_t *default_value = nullptr);
-}
+cbor_item_t *map_get(cbor_item_t *map, const std::string name,
+                     cbor_item_t *default_value = nullptr);
+}  // namespace cbor_helpers
--- src/cbor_map.cpp
+++ src/cbor_map.cpp
@@ -2,12 +2,12 @@
 #include <string.h>
 
 namespace cbor_helpers {
-cbor_item_t *map_get(cbor_item_t *map, const char *name,
+cbor_item_t *map_get(cbor_item_t *map, const std::string name,
                      cbor_item_t *default_value) {
   if (map != nullptr && cbor_map_is_definite(map)) {
     for (size_t i = 0; i < cbor_map_size(map); i++) {
-      if (strcmp(name, reinterpret_cast<char *>(cbor_string_handle(
-                           cbor_map_handle(map)[i].key))) == 0) {
+      if (strcmp(name.c_str(), reinterpret_cast<char *>(cbor_string_handle(
+                                   cbor_map_handle(map)[i].key))) == 0) {
         return cbor_map_handle(map)[i].value;
       }
     }
@@ -15,18 +15,19 @@ cbor_item_t *map_get(cbor_item_t *map, const char *name,
   return default_value;
 }
 
-bool map_has_str_value(cbor_item_t *map, const char *name, const char *value) {
+bool map_has_str_value(cbor_item_t *map, const std::string name,
+                       const std::string value) {
   cbor_item_t *item = map_get(map, name);
   if (item == nullptr) return 0;
-  if (cbor_string_length(item) != strlen(value)) return false;
-  if (strncmp(value, reinterpret_cast<char *>(cbor_string_handle(item)),
+  if (cbor_string_length(item) != value.length()) return false;
+  if (strncmp(value.c_str(), reinterpret_cast<char *>(cbor_string_handle(item)),
               cbor_string_length(item)) == 0) {
     return true;
   }
   return false;
 }
 
-const std::string map_get_str(cbor_item_t *map, const char *name,
+std::string map_get_str(cbor_item_t *map, const std::string name,
                               const std::string default_value) {
   cbor_item_t *value = map_get(map, name);
   if (value == nullptr) return default_value;
