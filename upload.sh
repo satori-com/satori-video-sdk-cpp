94df4242a945c98c9b7c028aff258cb90463548e
Oleg Khabinov <okhabinov@machinezone.com>
2017-08-15 12:13:13 -0700

    Update version to 0.1.6 and add Makefile

--- upload.sh
+++
@@ -1,4 +0,0 @@
-# for MAC use -s compiler.version=8.1 to fix compiler version detected by conan
-conan create satorivideo/master --build=missing
-conan remote add video http://10.199.28.20:80/; conan user --remote video -p ${CONAN_PASSWORD} ${CONAN_USER}
-conan upload --confirm --remote video --all '*@satorivideo/*' && echo 'SUCCESS'
