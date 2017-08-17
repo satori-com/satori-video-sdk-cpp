91aba70ff335302d322437868a0549fbcdd99834
Renat Idrisov <ridrisov@mz.com>
2017-08-17 12:58:12 +0200

    Limited execution and fixes

--- test_package/test.cpp
+++ test_package/test.cpp
@@ -2,21 +2,9 @@
 #include <librtmvideo/video_bot.h>
 #include <iostream>
 
-namespace verifier {
-void process_image(bot_context &context, const uint8_t *image, uint16_t width,
-                   uint16_t height, uint16_t linesize) {
-  std::cout << "got frame " << width << "x" << height << "\n";
-}
-cbor_item_t *process_command(bot_context &, cbor_item_t *) {
-  std::cout << "bot is initializing, libraries are ok" << '\n';
-  return nullptr;
-}
-}  // namespace verifier
-
-int main(int argc, char *argv[]) {
-  rtm_video_bot_register(bot_descriptor{640, 480, image_pixel_format::BGR,
-                                        &verifier::process_image,
-                                        &verifier::process_command});
+int main() {
+  rtm_video_bot_register(
+      bot_descriptor{640, 480, image_pixel_format::BGR, nullptr, nullptr});
   std::cout << "all good, includes and linking is fine, congrads!" << '\n';
   return 0;
 }
