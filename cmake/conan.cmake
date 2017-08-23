85d7c8f77337f65d133a50a0d1b9aed5c0db21e5
Renat Idrisov <ridrisov@mz.com>
2017-08-23 17:00:09 +0200

    Updating process for all existing bots

--- cmake/conan.cmake
+++
@@ -1,9 +0,0 @@
-set(CMAKE_CONAN_VERSION df45ac513c31e73ec1c401c2264f50d4c0ce2f9b)
-
-if(NOT EXISTS "${CMAKE_BINARY_DIR}/conan-${CMAKE_CONAN_VERSION}.cmake")
-    message(STATUS "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
-    file(DOWNLOAD "https://raw.githubusercontent.com/conan-io/cmake-conan/${CMAKE_CONAN_VERSION}/conan.cmake"
-            "${CMAKE_BINARY_DIR}/conan-${CMAKE_CONAN_VERSION}.cmake")
-endif()
-
-include(${CMAKE_BINARY_DIR}/conan-${CMAKE_CONAN_VERSION}.cmake)
