a05270b4834d71119790c40ea15846e01aba2a6a
Oleg Khabinov <okhabinov@machinezone.com>
2017-08-29 16:33:23 -0700

    AV utils test

---
+++ test/avutils_test.cpp
@@ -0,0 +1,17 @@
+#define BOOST_TEST_MODULE av utils
+#include <boost/test/included/unit_test.hpp>
+
+#include "../src/avutils.h"
+
+BOOST_AUTO_TEST_CASE(av_error_messages) {
+  BOOST_CHECK_EQUAL("Resource temporarily unavailable",
+                    rtm::video::avutils::error_msg(AVERROR(EAGAIN)));
+}
+
+BOOST_AUTO_TEST_CASE(av_pixel_format) {
+  BOOST_CHECK_EQUAL(AV_PIX_FMT_BGR24, rtm::video::avutils::to_av_pixel_format(
+                                          image_pixel_format::BGR));
+
+  BOOST_CHECK_EQUAL(AV_PIX_FMT_RGB0, rtm::video::avutils::to_av_pixel_format(
+                                         image_pixel_format::RGB0));
+}
