a409e4cac9ffa10b4549d5da4d100cf9c7487203
Mykhailo Aizatskyi <maizatskyi@mz.com>
2017-09-07 17:51:45 +0000

    CTEST_OUTPUT_ON_FAILURE=TRUE

--- conanfile.py
+++ conanfile.py
@@ -31,7 +31,7 @@ class SatorivideoConan(ConanFile):
         cmake = CMake(self)
         self.run('cmake . %s' % cmake.command_line)
         self.run("cmake --build . %s" % cmake.build_config)
-        self.run("make test")
+        self.run("CTEST_OUTPUT_ON_FAILURE=TRUE make test")
 
     def package(self):
         self.copy("*.h", dst="include", src="include")
