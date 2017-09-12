5e55bfc99d8c3eb53b18d7b79ed8fe7190b4effc
Renat Idrisov <ridrisov@mz.com>
2017-09-12 22:15:48 +0200

    Adding more comments

--- include/librtmvideo/video_bot.h
+++ include/librtmvideo/video_bot.h
@@ -43,10 +43,11 @@ EXPORT struct image_metadata {
   uint32_t plane_strides[MAX_IMAGE_PLANES];
 };
 
-// instance_data can be used to store data across multiple callbacks within a single bot instance
-// image_metadata contains frame size information
 EXPORT struct bot_context {
+  // instance_data can be used to store data across multiple callbacks
+  // within a single bot instance
   void *instance_data{nullptr};
+  // image_metadata contains frame size information
   const image_metadata *frame_metadata;
 };
 
@@ -81,7 +82,7 @@ EXPORT enum class bot_message_kind { ANALYSIS = 1, DEBUG = 2, CONTROL = 3 };
 
 // Sends bot implementation output to RTM subchannel.
 // id parameter is used to bind a message to a frame, by default a message is
-// binded to the current frame that is received by callback function
+// bound to the current frame that is received by the callback function
 EXPORT void rtm_video_bot_message(bot_context &context, const bot_message_kind kind,
                                   cbor_item_t *message,
                                   const frame_id &id = frame_id{0, 0});
