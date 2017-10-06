7e65c0729b58e36f5e4f3e5ce57dfcf1200ff066
Mike Aizatsky <maizatskyi@mz.com>
2017-10-06 09:12:24 -0700

    test fix

--- test_package/test.cpp
+++ test_package/test.cpp
@@ -2,8 +2,7 @@
 #include <iostream>
 
 int main() {
-  rtm_video_bot_register(
-      bot_descriptor{640, 480, image_pixel_format::BGR, nullptr, nullptr});
+  satori::video::bot_register({640, 480, image_pixel_format::BGR, nullptr, nullptr});
   std::cout << "all good, includes and linking is fine, congrats!" << '\n';
   return 0;
 }
