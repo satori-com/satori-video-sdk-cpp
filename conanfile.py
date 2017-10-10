511b85fe5c628c3bf88615d54d8320a65e30f05c
Mykhailo Aizatskyi <maizatskyi@mz.com>
2017-10-10 18:44:08 +0000

    trying out beast 122 (https://github.com/boostorg/beast/issues/807)

--- conanfile.py
+++ conanfile.py
@@ -12,7 +12,7 @@ class SatorivideoConan(ConanFile):
     requires = "Libcbor/0.5.0@satorivideo/master", \
                "Boost/1.64.0@satorivideo/master", \
                "Openssl/1.1.0f@satorivideo/master", \
-               "Beast/120@satorivideo/master", \
+               "Beast/122_experimental@satorivideo/master", \
                "Gsl/20017.07.27@satorivideo/master", \
                "Rapidjson/1.1.0@satorivideo/master", \
                "Ffmpeg/3.3.3_07@satorivideo/master", \
