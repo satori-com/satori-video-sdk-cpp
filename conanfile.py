f71e7ab68f3ff262be8501385aefe9b0e23a455a
Oleg Khabinov <okhabinov@machinezone.com>
2017-10-12 13:17:45 -0700

    Use correct version of Loguru

--- conanfile.py
+++ conanfile.py
@@ -16,7 +16,7 @@ class SatorivideoConan(ConanFile):
                "Gsl/20017.07.27@satorivideo/master", \
                "Rapidjson/1.1.0@satorivideo/master", \
                "Ffmpeg/3.3.3_07@satorivideo/master", \
-               "Loguru/1.5.1@satorivideo/master", \
+               "Loguru/1.5.0@satorivideo/master", \
                "SDL/2.0.5@satorivideo/master"
     license = "proprietary"
     version = '0.6.2'
