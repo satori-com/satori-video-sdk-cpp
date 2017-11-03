988be4df93a7d3b31dfcc6f0501dc08b2c0b6356
Oleg Khabinov <oleg.khabinov@gmail.com>
2017-11-03 14:21:05 -0700

    Call ctrl_callback even if config was not provided (#37)

--- test/bots/test_bot.cpp
+++ test/bots/test_bot.cpp
@@ -2,18 +2,34 @@
 #include <iostream>
 #include <thread>
 
-using namespace satori::video;
+#include "../../src/cbor_tools.h"
+
+namespace sv = satori::video;
 
 namespace test_bot {
 
-void process_image(bot_context &context, const image_frame & /*frame*/) {
+void process_image(sv::bot_context &context, const sv::image_frame & /*frame*/) {
   std::cout << "got frame " << context.frame_metadata->width << "x"
             << context.frame_metadata->height << "\n";
+}
+
+cbor_item_t *process_command(sv::bot_context &context, cbor_item_t *config) {
+  if (cbor::map_has_str_value(config, "action", "configure")) {
+    const cbor_item_t *body = cbor::map(config).get_map("body").item;
+    if (cbor_map_size(body) == 0) {
+      std::cout << "got no config\n";
+    } else {
+      std::cout << "processing config " << body << "\n";
+    }
+  }
+
+  return nullptr;
 }
 
 }  // namespace test_bot
 
 int main(int argc, char *argv[]) {
-  bot_register(bot_descriptor{image_pixel_format::BGR, &test_bot::process_image});
-  return bot_main(argc, argv);
+  sv::bot_register(sv::bot_descriptor{
+      sv::image_pixel_format::BGR, &test_bot::process_image, &test_bot::process_command});
+  return sv::bot_main(argc, argv);
 }
