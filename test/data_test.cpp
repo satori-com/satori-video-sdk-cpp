66c861c658471bef891726e3fd50f8e193d76e4a
Oleg Khabinov <oleg.khabinov@gmail.com>
2017-11-04 00:19:42 -0700

    Fix bug in encoded_metadata::to_network() (#40)

---
+++ test/data_test.cpp
@@ -0,0 +1,17 @@
+#define BOOST_TEST_MODULE DataTest
+#include <boost/test/included/unit_test.hpp>
+
+#include "data.h"
+
+namespace sv = satori::video;
+
+BOOST_AUTO_TEST_CASE(encoded_metadata_to_network) {
+  sv::encoded_metadata em;
+  em.codec_name = "dummy-codec";
+  em.codec_data = "dummy-codec-data";
+
+  const sv::network_metadata& nm = em.to_network();
+
+  BOOST_CHECK_EQUAL("dummy-codec", nm.codec_name);
+  BOOST_CHECK_EQUAL("ZHVtbXktY29kZWMtZGF0YQ==", nm.base64_data);
+}
