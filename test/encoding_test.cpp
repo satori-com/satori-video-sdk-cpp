9cf079dd071456e81bc37a46f5c5bfe19e78266c
Mike Aizatsky <mike.aizatsky@gmail.com>
2017-11-04 14:23:17 -0700

    simple base64 test (#39)

---
+++ test/encoding_test.cpp
@@ -0,0 +1,14 @@
+#define BOOST_TEST_MODULE EncodingTest
+#include <boost/test/included/unit_test.hpp>
+#include <gsl/gsl>
+
+#include "base64.h"
+
+using namespace satori::video;
+
+BOOST_AUTO_TEST_CASE(base64_encode) {
+  constexpr char plain[] = "\0\0\0\x01gM\0)\x80Kp\x10\x10\x1a ADT\0\0\0\x01h<";
+  std::string str = std::string(plain, sizeof(plain));
+  std::string result = encode64(str);
+  BOOST_CHECK_EQUAL("AAAAAWdNACmAS3AQEBogQURUAAAAAWg8AA==", result);
+}
