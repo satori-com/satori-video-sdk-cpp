fc175d5ed3146add92718584fb42ce76b6754a0c
Mike Aizatsky <mike.aizatsky@gmail.com>
2017-11-06 19:51:32 -0800

    version bump

--- conanfile.py
+++ conanfile.py
@@ -21,7 +21,7 @@ class SatorivideoConan(ConanFile):
                "PrometheusCpp/2017.11.03@satorivideo/master"
 
     license = "proprietary"
-    version = '0.9.1'
+    version = '0.9.2'
     settings = "os", "compiler", "build_type", "arch"
     default_options = "with_opencv=True", \
                       "with_gperftools=True", \
