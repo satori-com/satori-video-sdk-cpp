0667359a18ff8c500ca10b209a5e1508f8e782a8
Joe Malin <jmalin@mz.com>
2017-11-20 13:25:07 -0800

    TECHDOCS: Updated reference comments

--- include/satorivideo/opencv/opencv_bot.h
+++ include/satorivideo/opencv/opencv_bot.h
@@ -7,29 +7,42 @@ namespace satori {
 namespace video {
 
 //
-// Bot image callback receiving OpenCV Mat object.
-// This cv::Mat object does not own frame data, and it is guaranteed to live
-// only during callback execution frame. Perform deep clone
-// if you need to use it later.
+// Image handler callback function that accepts an OpenCV `Mat` object.
+// The object doesn't own the frame data it contains, and it's only valid
+// until you return from the callback. To keep a persistent copy, do a deep clone
+// and store the result in the bot_context.
 //
 using opencv_bot_img_callback_t =
     std::function<void(bot_context &context, const cv::Mat &img)>;
 
+//
+// Callback function that's invoked every time the bot framework finishes assembling an image
+//
 struct opencv_bot_descriptor {
-  // Invoked on every received image
   opencv_bot_img_callback_t img_callback;
 
-  // Invoked on every received control command, guaranteed to be invoked during
-  // initialization
+//
+// Callback function that's invoked every time the bot framework receives a message from the control
+// channel
+//
+// The bot invokes this function at least once, during initialization
+//
   bot_ctrl_callback_t ctrl_callback;
 };
 
-// Registers opencv bot.
-// Should be called by bot implementation before starting a bot.
+//
+// Registers your bot
+// Your code must call this function before calling `opencv_bot_main`.
+//
 EXPORT void opencv_bot_register(const opencv_bot_descriptor &bot);
 
-// Starts a bot (e.g. launches main event loop).
-// A bot implementation should be registered before calling this method.
+//
+// Starts your bot. The function starts the main event loop:
+// - Retrieves incoming video frames
+// - Passes the current frame to the video frame callback function
+// - Passes the current frame to the opencv callback function
+// Your code must call `opencv_bot_register` before calling this function
+//
 EXPORT int opencv_bot_main(int argc, char *argv[]);
 }  // namespace video
 }  // namespace satori
--- include/satorivideo/video_bot.h
+++ include/satorivideo/video_bot.h
@@ -1,15 +1,28 @@
+//
 // Video bot API.
-// To run a video bot:
-// - Call bot_register to register the bot with Satori
-// - Call bot_main to start the video bot main processing event loop.
+// The video bot API provides access to the Satori Video bot framework from a C++ bot.
+// The framework decodes incoming video streams and passes the resulting video frames to a callback function that
+// you define. In this function, you can analyze, transform, or replicate the frames. You can then publish the
+// results to an RTM channel.
+//
+// A video bot can also receive messages from an RTM channel called a **control channel**. Use this channel to
+// configure the bot.
+//
+// All video bots must call the following functions:
+// - `bot_register`: Registers the bot with the Satori Video bot framework
+// - `bot_main`: Starts the main event loop in the framework
+//
+// The main event loop decodes the video stream to produce video frames that it passes to your image processing
+// callback function.
 //
 // Example:
+// ```c++
 // int main(int argc, char *argv[]) {
 //   satori::video::bot_register(bot_descriptor{image_pixel_format::BGR,
 //                                              &transcoder::process_image});
 //   return satori::video::bot_main(argc, argv);
 // }
-//
+// ```
 // For more information, see the definitions of bot_register, bot_descriptor, and bot_main
 //
 
@@ -20,12 +33,16 @@
 
 #include "base.h"
 
+//
 // libcbor declarations
 // libcbor provides an API for CBOR-formatted messages, such as streaming video.
+//
 struct cbor_item_t;
 
+//
 // prometheus-cpp declarations
 // The Satori video bot API uses the Prometheus API for telemetry.
+//
 namespace prometheus {
 class Registry;
 }
@@ -33,33 +50,45 @@ class Registry;
 namespace satori {
 namespace video {
 
-// Every image belongs to a certain time interval, setting values wider
-// makes annotation applicable to multiple video frames
 //
-// Time-independent frame identifier provided by the video bot
+//
+// Frame identifier that's not time-related. The identifier is the sequence number of the frame in the stream.
+//
+// If i1 == i2, the id refers to a single frame.
+// If i1 < i2, the id refers to a range of frames, starting with frame i1 and ending with frame i2.
+//
+// When the bot framework invokes the image callback function, it passes an `image_frame` that contains a `frame_id`
+// and the image data. In this case, i1 == i2 and the id is for a single frame.
+//
+// The `bot_message` function has an `id` parameter in which you can pass a `frame_id` value that identifies the
+// frame or frames associated with the message.
 //
 EXPORT struct frame_id {
   int64_t i1;
   int64_t i2;
 };
 
-// Provides for as many as 4 separate planes for an image.
-// Packed RGB and packed YUV use plane 0.
+//
+// Provides for as many as 4 separate data planes for an image.
+// Packed formats such as packed RGB and packed YUV only use plane 0.
+//
 constexpr uint8_t MAX_IMAGE_PLANES = 4;
 
+//
 // If the video stream uses a packed pixel format such as packed RGB or packed YUV,
-// then it has only a single plane. All of the data is stored in plane_data[0].
+// then it has only a single data plane. All of the data is stored in `plane_data[0]`.
 // An image in planar pixel format such as planar YUV or HSV stores each component as a separate array (separate plane).
-// For example, with the planar YUV format, Y is plane_data[0], U is plane_data[1] and V is
-// plane_data[2].
-// A stride is a plane size with alignment.
+// For example, with the planar YUV format, Y is `plane_data[0]`, U is `plane_data[1]` and V is
+// `plane_data[2]`.
+//
 EXPORT struct image_frame {
   frame_id id;
   const uint8_t *plane_data[MAX_IMAGE_PLANES];
 };
 
 //
-// Describes the format of all images in the channel: image width, height, and number of plane strides per frame
+// Describes the contents of each frame: image width, height, and number of plane strides.
+// A plane stride is an aligned `plane_data`.
 //
 EXPORT struct image_metadata {
   uint16_t width;
@@ -67,21 +96,24 @@ EXPORT struct image_metadata {
   uint32_t plane_strides[MAX_IMAGE_PLANES];
 };
 
-// Indicators for controlling how your bot should react if the main event loop is running behind the rate of incoming
-// video messages.
-// `live`: Live video stream mode. The bot processes frames according to the frame rate of the input stream. If
-// processing takes longer, drop subsequent frames to stay in sync with the frame rate. Works with RTM channels,
-// camera input, and files.
-// Use live mode for production.
-// `batch`: Batch (test) input mode. The bot takes as much time as it needs to process a frame. No frames are dropped.
-// Only works with files.
-// Use `batch` mode for testing.
+//
+// Indicators for controlling how the main event loop decodes frames and hands them off to your image callback.
+//
+// - `live`: Live video stream mode. The bot framework sends frames to the image callback based on the
+// incoming frame rate. If the image callback lags behind, the bot framework drops frames to stay in sync with the frame
+// rate. This mode is available for RTM channel streams, camera input, and files.
+// **Use live mode for bots running in production.**
+//
+// -`batch`: Batch (test) input mode. The bot framework waits for the image callback to return before sending it another
+// frame, so no frames are dropped. This mode is only available for files.
+// **Only use `batch` mode for testing.**
+//
 EXPORT enum class execution_mode { LIVE = 1, BATCH = 2 };
 
 //
-// Persists global state for the main event loop.
+// Persists global state for your bot.
 // During the first iteration of the loop, bot_context is null.
-// Use `bot_context` instead of global storage.
+// **Use `bot_context` instead of global variables.**
 //
 EXPORT struct bot_context {
   // Global values
@@ -95,14 +127,18 @@ EXPORT struct bot_context {
 };
 
 //
-// Image handler callback function
+// Image frame processing callback function
 //
-// Defines the signature of the function that processes incoming images.
+// Defines the signature of the function that processes incoming frames.
 //
-// When you register a video bot, you pass a parameter that includes a pointer to your image analysis function.
-// For each iteration of the main event loop, the bot calls this function with the following parameters:
+// When you register a video bot, you pass a parameter that includes a pointer to an image processing function.
+// For each frame, the bot framework calls this function with the following parameters:
 // - context: A bot_context containing the current persistent context
 // - frame: An image_frame containing the next frame assembled from the video stream
+//
+// **NOTE:** In `live` execution mode, the bot framework may drop incoming frames in order to stay in sync with
+// the incoming frame rate.
+//
 using bot_img_callback_t =
     std::function<void(bot_context &context, const image_frame &frame)>;
 
@@ -111,13 +147,15 @@ using bot_img_callback_t =
 //
 // Defines the signature of the function that processes messages from the control channel.
 //
-// When you register a video bot, you pass a parameter that includes a pointer to your control channel function.
-// The video bot automatically subscribes to the control channel, and it invokes the function whenever a new control
-// message arrives. Use these messages to configure the video bot on startup and control its operation.
+// The bot framework automatically subscribes to a control channel associated with your bot.
+// When you register your video bot, you pass a parameter that includes a pointer to a function that the framework
+// invokes when it receives a control channel message. You can use these messages to configure the video bot on startup
+// and control its operation.
 //
-// This function is always invoked during initialization, even if the bot doesn't receive a message.
+// The framework always invokes this function during initialization. If there's no message in the channel at that
+// point, `message` is null.
 //
-// Although control channel messages can have any format, you should construct them as JSON objects with this format:
+// Construct messages JSON objects with this format:
 // { "action" : "configure",
 //   "body" : {
 //     <optional configuration parameters>
@@ -128,21 +166,21 @@ using bot_ctrl_callback_t =
     std::function<cbor_item_t *(bot_context &context, cbor_item_t *message)>;
 
 //
-// Indicators for specifying pixel format of frames in the incoming stream
+// Indicators for specifying the format of incoming images
 //
 enum class image_pixel_format { RGB0 = 1, BGR = 2 };
 
 //
-// Control structure that communicates your settings to the video bot
+// Control structure that communicates settings to the video bot framework
 //
 struct bot_descriptor {
   // Pixel format, like RGB0, BGR, etc.
   image_pixel_format pixel_format;
 
-  // Function to invoke whenever the video bot assembles a new frame from the incoming video stream
+  // Function to invoke whenever the bot framework assembles a new frame from the incoming video stream
   bot_img_callback_t img_callback;
 
-  // Function to invoke whenever the video bot receives a control channel message. The bot always invokes this
+  // Function to invoke whenever the bot framework receives a control channel message. The bot always invokes this
   // function during bot initialization, even if it doesn't receive a messsage
   bot_ctrl_callback_t ctrl_callback;
 };
@@ -150,24 +188,30 @@ struct bot_descriptor {
 // Indicators that specify which channel to use for a message
 EXPORT enum class bot_message_kind { ANALYSIS = 1, DEBUG = 2, CONTROL = 3 };
 
+//
 // Publishes a message to one of the subchannels for this bot.
 //
 // All of the data you send is aggregated and published as a single message at the end of each main event loop
 // iteration (after the image callback function returns).
 //
-// The id parameter specifies the frame associated with the message. By default, the id value is frame_id{0,0}, which
-// indicates that the message is for the current frame.
+// The id parameter specifies the frame or frames associated with the message. By default, the id value is
+// frame_id{0,0}, which indicates that the message is for the current frame. Set `frame_id.id1` == `frame_id.id2` to
+// refer to a single frame, or for a range of frames, set `frame_id.id1` to the first frame in the range, and
+// `frame_id.id2` to the last frame in the range.
 //
-// See rtmvideo.md to learn more about the channels provides by the Video Stream platform.
 EXPORT void bot_message(bot_context &context, bot_message_kind kind, cbor_item_t *message,
                         const frame_id &id = frame_id{0, 0});
 
+//
 // Registers the bot, including your settings
 // Call this function before you start the main event loop.
+//
 EXPORT void bot_register(const bot_descriptor &bot);
 
+//
 // Starts the bot (launches the main event loop).
 // Remember to register your bot before calling this function
+//
 EXPORT int bot_main(int argc, char *argv[]);
 
 }  // namespace video
