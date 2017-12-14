4aa604c2a62c2ca1a9d35bb60b6961d0cf2cf882
Oleg Khabinov <oleg.khabinov@gmail.com>
2017-12-14 08:04:03 -0800

    Bump prometheus version (#169)

--- conanfile.py
+++ conanfile.py
@@ -17,11 +17,11 @@ class SatorivideoConan(ConanFile):
                "Libcbor/0.5.0@satorivideo/master", \
                "Loguru/1.5.0@satorivideo/master", \
                "Openssl/1.1.0g@satorivideo/master", \
-               "PrometheusCpp/2017.12.06@satorivideo/master", \
+               "PrometheusCpp/2017.12.13@satorivideo/master", \
                "SDL/2.0.5@satorivideo/master"
 
     license = "proprietary"
-    version = '0.10.25'
+    version = '0.10.26'
     settings = "os", "compiler", "build_type", "arch"
     default_options = "with_opencv=True", \
                       "with_gperftools=True", \
