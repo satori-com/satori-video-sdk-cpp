bc762d68366b5730f29f31e6469200e42b524101
Renat Idrisov <ren@ngs.ru>
2017-12-14 15:25:15 +0100

    Adding description for helper functions (#164)
    * Adding description for helper functions
    * Typos and fixes

--- include/satorivideo/opencv/opencv_utils.h
+++ include/satorivideo/opencv/opencv_utils.h
@@ -10,8 +10,16 @@ namespace satori {
 namespace video {
 namespace opencv {
 
+// It is recommended to publish coordinates as fractions of the full frame size.
+// For example, if the frame is 100x100,
+// fractional coordinates of the point (20, 30) will be (0.2, 0.3)
+// following conversion functions are explained with examples
+
+// Example: it converts from (64, 48, 64, 48) -> (0.1, 0.1, 0.1, 0.1) for size 640x480
 cv::Rect2d convert_to_fractional(const cv::Rect &rect, const cv::Size &view);
+// Example: it converts from  (0.1, 0.1) -> (64, 48) for size 640x480
 cv::Point2d convert_from_fractional(const cv::Point2d &p, const cv::Size &view);
+// Example: it converts from  (0.1, 0.1, 0.1, 0.1) -> (64, 48, 64, 48) for size 640x480
 cv::Rect2d convert_from_fractional(const cv::Rect2d &p, const cv::Size &view);
 
 // CBOR output functions
@@ -22,7 +30,12 @@ cbor_item_t *rect_to_cbor(cv::Rect2d rect);
 cv::Rect2d rect_from_cbor(cbor_item_t *item);
 cv::Point2d point_from_cbor(cbor_item_t *item);
 
-// Debugging functions
+// Saves given image as file "logs/frame<number>.jpg", number starts from 1 and
+// increases on every function call.
+// It will also produce error message if folder "logs" does not exist.
+// This function could be helpful for local debugging
+// For example, three calls will produce three files:
+// logs/frame1.jpg, logs/frame2.jpg and logs/frame3.jpg
 void log_image(const cv::Mat &image);
 
 }  // namespace opencv
