ab730e9e5d9acf1fd64d6a0ea9891a152eac9324
Oleg Khabinov <oleg.khabinov@gmail.com>
2017-12-14 08:43:50 -0800

    Add jitter (#170)

--- src/video_streams.cpp
+++ src/video_streams.cpp
@@ -1,4 +1,6 @@
-#include <boost/bind.hpp>
+#include <boost/accumulators/accumulators.hpp>
+#include <boost/accumulators/statistics.hpp>
+#include <boost/accumulators/statistics/rolling_window.hpp>
 #include <iostream>
 
 #include "base64.h"
@@ -11,6 +13,8 @@ namespace satori {
 namespace video {
 
 namespace {
+
+namespace accum = boost::accumulators;
 
 auto &frame_chunks_mismatch = prometheus::BuildCounter()
                                   .Name("network_decoder_frame_chunks_mismatch")
@@ -31,6 +35,14 @@ auto &frame_timestamp_delta_millis =
                                      10, 15,  20,  25,  30,  40,  50,  60,  70, 80,
                                      90, 100, 200, 300, 400, 500, 750, 1000});
 
+auto &frame_timestamp_jitter =
+    prometheus::BuildHistogram()
+        .Name("frame_timestamp_jitter")
+        .Register(metrics_registry())
+        .Add({}, std::vector<double>{0,  1,   2,   3,   4,   5,   6,   7,  8,  9,
+                                     10, 15,  20,  25,  30,  40,  50,  60, 70, 80,
+                                     90, 100, 150, 200, 250, 300, 400, 500});
+
 auto &frame_arrival_time_delta_millis =
     prometheus::BuildHistogram()
         .Name("frame_arrival_time_delta_millis")
@@ -38,6 +50,14 @@ auto &frame_arrival_time_delta_millis =
         .Add({}, std::vector<double>{0,  1,   2,   3,   4,   5,   6,   7,   8,  9,
                                      10, 15,  20,  25,  30,  40,  50,  60,  70, 80,
                                      90, 100, 200, 300, 400, 500, 750, 1000});
+
+auto &frame_arrival_time_jitter =
+    prometheus::BuildHistogram()
+        .Name("frame_arrival_time_itter")
+        .Register(metrics_registry())
+        .Add({}, std::vector<double>{0,  1,   2,   3,   4,   5,   6,   7,  8,  9,
+                                     10, 15,  20,  25,  30,  40,  50,  60, 70, 80,
+                                     90, 100, 150, 200, 250, 300, 400, 500});
 
 }  // namespace
 
@@ -53,6 +73,8 @@ streams::op<network_packet, encoded_packet> decode_network_stream() {
 
     streams::publisher<encoded_packet> operator()(const network_frame &nf) {
       if (_chunk != nf.chunk) {
+        LOG(ERROR) << "chunk mismatch f.id=" << nf.id << " expected " << _chunk
+                   << ", got " << nf.chunk;
         frame_chunks_mismatch.Increment();
         reset();
         return streams::publishers::empty<encoded_packet>();
@@ -113,15 +135,25 @@ streams::op<encoded_packet, encoded_packet> report_frame_dynamics() {
       if (_first_frame) {
         _first_frame = false;
       } else {
-        frame_id_deltas.Observe(std::abs(f.id.i1 - _last_frame_id.i1));
-        frame_timestamp_delta_millis.Observe(
+        const double timestamp_delta =
             std::abs(std::chrono::duration_cast<std::chrono::milliseconds>(
                          f.timestamp - _last_frame_timestamp)
-                         .count()));
-        frame_arrival_time_delta_millis.Observe(
+                         .count());
+        const double arrival_time_delta =
             std::abs(std::chrono::duration_cast<std::chrono::milliseconds>(
                          arrival_time - _last_frame_arrival_time)
-                         .count()));
+                         .count());
+
+        frame_id_deltas.Observe(std::abs(f.id.i1 - _last_frame_id.i1));
+        frame_timestamp_delta_millis.Observe(timestamp_delta);
+        frame_arrival_time_delta_millis.Observe(arrival_time_delta);
+
+        _timestamp_variance_accum(timestamp_delta);
+        _arrival_time_variance_accum(arrival_time_delta);
+
+        frame_timestamp_jitter.Observe(standard_deviation(_timestamp_variance_accum));
+        frame_arrival_time_jitter.Observe(
+            standard_deviation(_arrival_time_variance_accum));
       }
 
       _last_frame_id = f.id;
@@ -130,10 +162,23 @@ streams::op<encoded_packet, encoded_packet> report_frame_dynamics() {
     }
 
    private:
+    // TODO: prometheus can probably calculate standard deviation on it's own.
+    double standard_deviation(
+        accum::accumulator_set<double, accum::stats<accum::tag::variance>> &accumulator) {
+      auto n = static_cast<double>(accum::count(accumulator));
+      return std::sqrt((accum::variance(accumulator) * n) / n - 1);
+    }
+
     bool _first_frame{true};
     frame_id _last_frame_id;
     std::chrono::system_clock::time_point _last_frame_timestamp;
     std::chrono::system_clock::time_point _last_frame_arrival_time;
+
+    // TODO: might be nicer to have sliding time windows
+    accum::accumulator_set<double, accum::stats<accum::tag::variance>>
+        _timestamp_variance_accum{accum::tag::rolling_window::window_size = 25};
+    accum::accumulator_set<double, accum::stats<accum::tag::variance>>
+        _arrival_time_variance_accum{accum::tag::rolling_window::window_size = 25};
   };
 
   return [](streams::publisher<encoded_packet> &&src) {
