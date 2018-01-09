3c39fa7a166f11243513837dbcf57867ffa3f5d6
Mike Aizatsky <mike.aizatsky@gmail.com>
2018-01-08 16:08:23 -0800

    nit

--- src/tcmalloc.h
+++ src/tcmalloc.h
@@ -8,13 +8,13 @@ namespace satori {
 namespace video {
 
 #ifdef HAS_GPERFTOOLS
-void init_tcmalloc() {
+inline void init_tcmalloc() {
   // Touch tcmalloc code to initialize when it is statically linked on Mac.
   // http://www.awenius.de/blog/2014/10/02/the-tcmalloc-heap-profiler-does-not-work-when-tcmalloc-is-statically-linked/
   IsHeapProfilerRunning();
 }
 #else
-void init_tcmalloc() { }
+inline void init_tcmalloc() { }
 #endif
 
 }
